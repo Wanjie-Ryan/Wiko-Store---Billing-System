4cf444384257fc40e7a825a78da2493346978301
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-18 15:32:20 +0300

    Creating database connection to the categories buttons

--- Logics/CategoryLogics.cs
+++ Logics/CategoryLogics.cs
@@ -8,8 +8,10 @@ namespace Wiko_Store.Logics
 {
     internal class CategoryLogics
     {
-        public int categoryId { get; set; }
+        public int id { get; set; }
         public string title { get; set; }
         public string description { get; set; }
+        public DateTime added_date { get; set; }
+        public int added_by { get; set; }
     }
 }
