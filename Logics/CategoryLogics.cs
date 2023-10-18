e50915f2de48f34ffc708b2653228f24d44bdd6b
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-18 15:27:50 +0300

    Categories Logics

---
+++ Logics/CategoryLogics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiko_Store.Logics
+{
+    internal class CategoryLogics
+    {
+        public int categoryId { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+    }
+}
