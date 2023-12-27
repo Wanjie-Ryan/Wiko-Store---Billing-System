2e0ce345e245c71a188e10ac87b89decf70b9619
Ryan <ryanwanjiedistortion@gmail.com>
2023-12-27 13:50:03 +0300

    Database manipulation for products module

---
+++ Data Layer/productsDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiko_Store.Data_Layer
+{
+    internal class productsDAL
+    {
+        //creating a static string method for DB connection
+
+
+    }
+}
