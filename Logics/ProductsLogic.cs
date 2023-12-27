d2f0db5ed315521d54bd99a7ba3df3db82616a1e
Ryan <ryanwanjiedistortion@gmail.com>
2023-12-27 13:47:47 +0300

    getters and setters for products module

---
+++ Logics/ProductsLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiko_Store.Logics
+{
+    internal class ProductsLogic
+    {
+        //contains getters and setters for products
+        public int id { get; set; }
+        public string name { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+        public decimal rate { get; set; }
+        public decimal quantity { get; set; }
+        public DateTime added_date { get; set; }
+        public int added_by { get; set; }
+    }
+}
