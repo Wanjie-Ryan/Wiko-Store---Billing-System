5d6331ffa0fce9fcb24f21b4a10c72d5b25fe167
Ryan <ryanwanjiedistortion@gmail.com>
2023-12-31 12:53:17 +0300

    Database manipulation

---
+++ Data Layer/CustomerDAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wiko_Store.Data_Layer
+{
+    internal class CustomerDAL
+    {
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+
+        public DataTable Select()
+        {
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+    }
+}
