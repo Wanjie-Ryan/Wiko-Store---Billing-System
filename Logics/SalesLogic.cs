b78153168b9a9ed951270f6429d17e21e13868ae
Ryan <ryanwanjiedistortion@gmail.com>
2024-01-02 12:54:17 +0300

    logic layer for transactions and transaction details

---
+++ Logics/SalesLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiko_Store.Logics
+{
+    internal class SalesLogic
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public int dealer_cust_id { get; set; }
+        public decimal total { get; set; }
+        public DateTime transaction_date { get; set; }
+        public decimal tax { get; set; }
+        public decimal discount { get; set; }
+        public int added_by { get; set; }
+    }
+}
