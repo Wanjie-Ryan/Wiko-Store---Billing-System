6ef14a9dd230ddf3c19cbfdb300a4813967f20c3
Ryan <ryanwanjiedistortion@gmail.com>
2024-01-02 13:00:51 +0300

    creating the logics for transaction and transaction details

---
+++ Logics/transactionDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiko_Store.Logics
+{
+    internal class transactionDetails
+    {
+        public int id { get; set; }
+        public int product_id { get; set; }
+        public decimal rate { get; set; }
+        public decimal quantity { get; set; }
+        public decimal total { get; set; }
+        public int dea_cust_id { get; set; }
+        public DateTime added_date { get; set; }
+        public int added_by { get; set; }
+
+    }
+}
