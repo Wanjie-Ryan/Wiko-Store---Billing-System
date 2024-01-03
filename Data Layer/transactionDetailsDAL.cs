ca04ed24bdd1f00b37185e5d7359666fc9388682
Ryan <ryanwanjiedistortion@gmail.com>
2024-01-03 08:56:19 +0300

    Database manipulation, insertion via the transactiondetails

--- Data Layer/transactionDetailsDAL.cs
+++ Data Layer/transactionDetailsDAL.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wiko_Store.Logics;
 
 namespace Wiko_Store.Data_Layer
 {
     internal class transactionDetailsDAL
     {
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+
+        public bool InsertTransDetails(transactionDetailsLogics tl)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+
+            return isSuccess;
+        }
     }
 }
