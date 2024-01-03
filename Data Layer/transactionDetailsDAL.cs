6fd445391526ec5d0565bf328b84a62c266248e3
Ryan <ryanwanjiedistortion@gmail.com>
2024-01-03 10:19:46 +0300

    Database functionality, insertion functionality

--- Data Layer/transactionDetailsDAL.cs
+++ Data Layer/transactionDetailsDAL.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Wiko_Store.Logics;
 
 namespace Wiko_Store.Data_Layer
@@ -22,16 +23,38 @@ namespace Wiko_Store.Data_Layer
 
             try
             {
+                string sql = "INSERT INTO tbl_transaction_detail (product_id, rate, quantity, total, dea_cust_id, added_date, added_by) VALUES (@product_id, @rate, @quantity, @total, @dea_cust_id, @added_date, @added_by)";
 
+                SqlCommand cmd = new SqlCommand(sql, conn);
 
+                cmd.Parameters.AddWithValue("@product_id", tl.product_id);
+                cmd.Parameters.AddWithValue("@rate", tl.rate);
+                cmd.Parameters.AddWithValue("@quantity", tl.quantity);
+                cmd.Parameters.AddWithValue("@total", tl.total);
+                cmd.Parameters.AddWithValue("@dea_cust_id", tl.dea_cust_id);
+                cmd.Parameters.AddWithValue("@added_date", tl.added_date);
+                cmd.Parameters.AddWithValue("@added_by", tl.added_by);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
             }
             catch(Exception ex)
             {
-
+                MessageBox.Show(ex.Message);
             }
             finally
             {
-
+                conn.Close();
             }
 
             return isSuccess;
